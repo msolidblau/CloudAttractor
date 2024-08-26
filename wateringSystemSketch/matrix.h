ca0847eb590351da6bab0d738f2391f21928c607
msolidblau <m@savall.eu>
2024-08-25 19:06:41 -0700

    add matrix flies and individual zones files

---
+++ wateringSystemSketch/matrix.h
@@ -0,0 +1,52 @@
+#include "pinDef.h"
+#include "Arduino_LED_Matrix.h"
+
+
+const uint32_t zone4[][4] = {
+	{
+		0xfa412,
+		0x423e4048,
+		0x4f84000,
+		66
+	}
+};
+
+
+const uint32_t zone3[][4] = {
+	{
+		0xf9e10,
+		0x22044088,
+		0x4f9c000,
+		66
+	}
+};
+  
+
+const uint32_t zone2[][4] = {
+	{
+		0xf8e11,
+		0x12024048,
+		0x8f9f000,
+		66
+	}
+};
+
+
+const uint32_t zone1[][4] = {
+	{
+		0xf8610,
+		0xa2124028,
+		0x2f8f000,
+		66
+	}
+};
+
+
+const uint32_t off[][4] = {
+	{
+		0x7198b,
+		0x38a28bb8,
+		0xa2722000,
+		66
+	}
+};
